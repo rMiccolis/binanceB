972253e3946d37d7f285161f219f31a6fd0e4d78
rMiccolis <r.miccolis1@gmail.com>
2022-05-20 20:01:19 +0200

    aggiustamenti interfaccia: impossibile usare v-data-table vuetify

--- client/src/views/home.view.vue
+++ client/src/views/home.view.vue
@@ -1,19 +1,41 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from "vue";
+
+let headers = ref([
+  {text: 'asset', align: 'start', sortable: true, value: 'asset', },
+  {text: 'free', align: 'start', sortable: true, value: 'free'},
+  {text: 'locked', align: 'start', sortable: true, value: 'locked'}
+])
+
+let search = ref('')
+
+let balances =ref([ { 'asset': 'BNB', 'free': '1000.70000000', 'locked': '0.00000000' }, { 'asset': 'BTC', 'free': '1.00000000', 'locked': '0.00000000' }, { 'asset': 'BUSD', 'free': '10000.00000000', 'locked': '0.00000000' }, { 'asset': 'ETH', 'free': '100.00000000', 'locked': '0.00000000' }, { 'asset': 'LTC', 'free': '500.00000000', 'locked': '0.00000000' }, { 'asset': 'TRX', 'free': '500000.00000000', 'locked': '0.00000000' }, { 'asset': 'USDT', 'free': '9800.60500000', 'locked': '0.00000000' }, { 'asset': 'XRP', 'free': '50000.00000000', 'locked': '0.00000000' } ])
 
 </script>
 
 <template>
-  <div class="prova">
-    <button class="btn btn-info">wallet</button>
-    <!-- <sidebar :open="open" /> -->
-  </div>
+    
+    <v-card>
+      <v-card-title>
+        Nutrition
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="balances"
+        :search="search"
+        :items-per-page="5"
+      ></v-data-table>
+    </v-card>
 </template>
 
 <style scoped>
-.prova {
-  align-content: center;
-  justify-content: center;
-  align-content: center;
-}
+
 </style>
--- client/src/views/wallet.view.vue
+++ client/src/views/wallet.view.vue
@@ -8,19 +8,34 @@
           <v-card-title>
             <v-col cols="9">Wallet Info</v-col>
             <v-col
-              ><v-btn class="mx-2" @click="getAccountInfo()" fab dark small rounded color="blue">
-               <i class="bi bi-arrow-clockwise"></i>
-              </v-btn></v-col
-            ></v-card-title
-          >
+              ><v-btn
+                class="mx-2"
+                @click="getAccountInfo()"
+                fab
+                dark
+                small
+                rounded
+                color="blue"
+              >
+                <i class="bi bi-arrow-clockwise"></i> </v-btn></v-col
+          ></v-card-title>
           <v-divider></v-divider>
           <v-row class="pa-2" no-gutters>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title><h4>data: </h4></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            {{accountInfo}}
+            <v-col cols="12">
+              <h2>Data:</h2>
+            </v-col>
+            <div v-for="(item, key) in accountInfo">
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <h4>{{ key }}:</h4>
+                  </v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    item
+                  }}</v-list-item-subtitle>                
+                </v-list-item-content>
+              </v-list-item>
+            </div>
           </v-row>
         </v-card>
       </v-col>
@@ -30,18 +45,21 @@
 
 <script setup>
 import { ref, watch } from "vue";
-import axios from 'axios';
+import axios from "axios";
 
 let accountInfo = ref(null);
-let userId = 'test'
+let userId = "test";
 
 let getAccountInfo = async () => {
-  accountInfo.value = await axios({
-    method: "get",
-    url: "http://localhost:3000/api/account",
-    params: {userId} 
-  });
-}
+  // accountInfo.value = (
+  //   await axios({
+  //     method: "get",
+  //     url: "http://localhost:3000/api/account",
+  //     params: { userId },
+  //   })
+  // ).data;
+  accountInfo.value = {"makerCommission":0,"takerCommission":0,"buyerCommission":0,"sellerCommission":0,"canTrade":true,"canWithdraw":false,"canDeposit":false,"updateTime":1652523293213,"accountType":"SPOT","balances":[{"asset":"BNB","free":"1000.70000000","locked":"0.00000000"},{"asset":"BTC","free":"1.00000000","locked":"0.00000000"},{"asset":"BUSD","free":"10000.00000000","locked":"0.00000000"},{"asset":"ETH","free":"100.00000000","locked":"0.00000000"},{"asset":"LTC","free":"500.00000000","locked":"0.00000000"},{"asset":"TRX","free":"500000.00000000","locked":"0.00000000"},{"asset":"USDT","free":"9800.60500000","locked":"0.00000000"},{"asset":"XRP","free":"50000.00000000","locked":"0.00000000"}],"permissions":["SPOT"]}
+};
 </script>
 
 <style scoped>

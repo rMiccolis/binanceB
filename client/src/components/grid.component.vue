15f6d7d12d26aa18397e474c425a4ba0d0b50a1f
rMiccolis <r.miccolis1@gmail.com>
2022-06-14 17:48:43 +0200

    aggiunto modal info in grid component

--- client/src/components/grid.component.vue
+++ client/src/components/grid.component.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <!-- <v-row class="pa-2" no-gutters>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-title @click="getStakingInfo()">
-            Staking:
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-row> -->
-
     <div v-if="data?.length > 0">
       <v-table density="default">
         <thead>
@@ -24,11 +11,16 @@
             >
               {{ key }}
             </th>
+            <th>Details</th>
           </tr>
         </thead>
         <tbody v-if="data">
           <tr v-for="(item, key) in data" :key="key">
             <td v-for="(elem, name) in item" :key="name">{{ elem }}</td>
+            <td>
+              <v-icon @click="toggleModal(key)" color="blue" class="cursor-pointer">mdi-information-outline</v-icon>
+              
+            </td>
           </tr>
         </tbody>
       </v-table>
@@ -37,6 +29,28 @@
       <div v-if="data && data?.length == 0">No Data Available!</div>
       <div v-else-if="!data">Loading Staking...</div>
     </div>
+    <v-dialog
+        v-model="openModal"
+        scrollable
+      >
+      <v-card>
+        <v-card-title>Details:</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          {{detailShown}}
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+            color="blue-darken-1"
+            text
+            @click="openModal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      </v-dialog>
   </div>
 </template>
 
@@ -46,8 +60,21 @@ import axios from "axios";
 
 const props = defineProps({
   data: { type: Array, default: null, required: false },
+  detailData: { type: Array, default: null, required: false },
 });
+
+let openModal = ref(false)
+let detailShown = ref({})
+
+let toggleModal = (key) => {
+  detailShown.value = props.data[key]
+  openModal.value = true
+}
+
 </script>
 
 <style scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
 </style>

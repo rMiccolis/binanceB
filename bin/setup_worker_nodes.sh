d048c13ed4ac1e5f33f1e97f6d44a8cfec5aac3c
rMiccolis <r.miccolis1@gmail.com>
2023-04-23 18:21:53 +0200

    fix running script on workers

--- bin/setup_worker_nodes.sh
+++ bin/setup_worker_nodes.sh
@@ -7,15 +7,37 @@ for h in ${hosts[@]}; do
   IFS='@' read -r -a host_string <<< "$h"
   host_username=${host_string[0]}
   host_ip=${host_string[1]}
-  ssh-keyscan $host_ip >> ~/.ssh/known_hosts
-  scp /home/binanceB/bin/infrastructure_setup.sh $h:/home/$host_username/
+  echo -e "${LCYAN}LOOPING ON $host_username@$host_ip"
+  ssh-keyscan $host_ip >> ~/.ssh/known_hosts &
+  wait
+  scp /home/binanceB/bin/infrastructure_setup.sh $h:/home/$host_username/ &
+  wait
   # add github to the list of known_hosts addresses
-  ssh -A $h "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+  ssh -A $h "ssh-keyscan github.com >> ~/.ssh/known_hosts" &
+  wait
   # clone github repository code 
-  ssh -A $h "git clone --single-branch --branch develop git@github.com:rMiccolis/binanceB.git"
-  ssh -A $h "./binanceB/bin/set_host_settings.sh"
-  ssh -A $h "./binanceB/bin/install_docker.sh"
-  ssh -A $h "./binanceB/bin/install_cri_docker.sh"
-  ssh -A $h "./binanceB/bin/install_kubernetes.sh"
+  echo -e "${LCYAN}Cloning repository"
+  ssh -A $h "git clone --single-branch --branch develop git@github.com:rMiccolis/binanceB.git" &
+  wait
+
+  echo -e "${LCYAN}set_host_settings.sh"
+  ssh -A $h "/home/binanceB/bin/set_host_settings.sh" &
+  wait
+
+  echo -e "${LCYAN}install_docker.sh"
+  ssh -A $h "/home/binanceB/bin/install_docker.sh" &
+  wait
+
+  echo -e "${LCYAN}install_cri_docker.sh"
+  ssh -A $h "/home/binanceB/bin/install_cri_docker.sh" &
+  wait
+
+  echo -e "${LCYAN}install_kubernetes.sh" &
+  wait
+  ssh -A $h "/home/binanceB/bin/install_kubernetes.sh" &
+  wait
+
+  echo -e "${LCYAN}Joining cluster.sh" &
+  wait
   ssh -A $h "$join"
 done

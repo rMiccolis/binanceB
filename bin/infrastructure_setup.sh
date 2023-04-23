064389193f885a24878c8fbf5b8e19f600ff8828
rMiccolis <r.miccolis1@gmail.com>
2023-04-23 17:47:14 +0200

    WIP: consolidamento script

--- bin/infrastructure_setup.sh
+++ bin/infrastructure_setup.sh
@@ -60,32 +60,37 @@ echo -e "${GREEN}Cloning private repository: ===> git@github.com:rMiccolis/binan
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 # clone github repository code 
 git clone --single-branch --branch develop git@github.com:rMiccolis/binanceB.git
-chmod u+x ./binanceB/bin/set-host-settings.sh
-chmod u+x ./binanceB/bin/install-docker.sh
-chmod u+x ./binanceB/bin/install-cri-docker.sh
-chmod u+x ./binanceB/bin/install-kubernetes.sh
-chmod u+x ./binanceB/bin/install-helm.sh
-chmod u+x ./binanceB/bin/install-nginx.sh
-chmod u+x ./binanceB/bin/install-app.sh
+chmod u+x ./binanceB/bin/set_host_settings.sh
+chmod u+x ./binanceB/bin/install_docker.sh
+chmod u+x ./binanceB/bin/install_cri_docker.sh
+chmod u+x ./binanceB/bin/install_kubernetes.sh
+chmod u+x ./binanceB/bin/init_kubernetes_cluster.sh
+chmod u+x ./binanceB/bin/setup_worker_nodes.sh
+chmod u+x ./binanceB/bin/install_helm.sh
+chmod u+x ./binanceB/bin/install_nginx.sh
+chmod u+x ./binanceB/bin/install_app.sh
 cd binanceB
 
 echo -e "${GREEN}Starting phase 1 ===> Setting up host settings and dependencies: $(hostname -I)${WHITE}"
 ./bin/set_host_settings.sh
+
 echo -e "${GREEN}Starting phase 2 ===> Installing Docker${WHITE}"
 ./bin/install_docker.sh -u $docker_username -p $docker_password
+
 echo -e "${GREEN}Starting phase 3 ===> Installing Cri-Docker (Container Runtime Interface)${WHITE}"
 ./bin/install_cri_docker.sh
+
 echo -e "${GREEN}Starting phase 4 ===> Installing Kubernetes${WHITE}"
 ./bin/install_kubernetes.sh
+
 echo -e "${GREEN}Starting phase 5 ===> Initialize Kubernetes cluster${WHITE}"
 ./bin/init_kubernetes_cluster.sh
 
-echo -e "${GREEN}Starting phase 6 ===> Setup worker nodes (setting worker dependencies) ${WHITE}"
+echo -e "${GREEN}Starting phase 6 ===> Setup worker nodes and joining them to cluster ${WHITE}"
 ./bin/setup_worker_nodes.sh
-
-echo -e "${GREEN}Starting phase 6 ===> Join workers to Kubernetes cluster${WHITE}"
 
 echo -e "${GREEN}Starting phase 2 ===> Installing Helm (package manager for Kubernetes)${WHITE}"
 ./bin/install_helm.sh
+
 echo -e "${GREEN}Starting phase 2 ===> Installing Nginx (to be used as a reverse proxy for Kubernetes cluster)${WHITE}"
 ./bin/install_nginx.sh
--- bin/setup_worker_nodes.sh
+++ bin/setup_worker_nodes.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 join=$(kubeadm token create --print-join-command)
+touch ~/.ssh/known_hosts
 for h in ${hosts[@]}; do
   host_string=()
   IFS='@' read -r -a host_string <<< "$h"
   host_username=${host_string[0]}
   host_ip=${host_string[1]}
+  ssh-keyscan $host_ip >> ~/.ssh/known_hosts
   scp /home/binanceB/bin/infrastructure_setup.sh $h:/home/$host_username/
-  ssh -A $h
-  chmod u+x ./infrastructure_setup.sh
+  # add github to the list of known_hosts addresses
+  ssh -A $h "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+  # clone github repository code 
+  ssh -A $h "git clone --single-branch --branch develop git@github.com:rMiccolis/binanceB.git"
+  ssh -A $h "cd binanceB"
+  ssh -A $h "./bin/set_host_settings.sh"
+  ssh -A $h "./bin/install_docker.sh"
+  ssh -A $h "./bin/install_cri_docker.sh"
+  ssh -A $h "./bin/install_kubernetes.sh"
+  ssh -A $h "$join"
 done

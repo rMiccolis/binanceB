056a43e3675240dbcb8d454c5c919d787bd47d3c
rMiccolis <r.miccolis1@gmail.com>
2023-04-26 16:13:41 +0200

    fix reading of data in json

--- bin/install_app.sh
+++ bin/install_app.sh
@@ -2,13 +2,14 @@
 
 ###############################################################################
 # Configuring application settings
-export cluster_dns_name="$(jq -r '.cluster_dns_name | @sh' $config_file_path)"
-server_access_token_secret="$(jq -r '.server_access_token_secret | @sh' $config_file_path | base64 --decode)"
-server_refresh_token_secret="$(jq -r '.server_refresh_token_secret | @sh' $config_file_path | base64 --decode)"
-server_access_token_lifetime="$(jq -r '.server_access_token_lifetime | @sh' $config_file_path)"
-server_refresh_token_lifetime="$(jq -r '.server_refresh_token_lifetime | @sh' $config_file_path)"
-mongo_root_username="$(jq -r '.mongo_root_username | @sh' $config_file_path | base64 --decode)"
-mongo_root_password="$(jq -r '.mongo_root_password | @sh' $config_file_path | base64 --decode)"
+eval cluster_dns_name="$(jq -r '.cluster_dns_name | @sh' $config_file_path)"
+export cluster_dns_name
+eval server_access_token_secret="$(jq -r '.server_access_token_secret | @sh' $config_file_path | base64 --decode)"
+eval server_refresh_token_secret="$(jq -r '.server_refresh_token_secret | @sh' $config_file_path | base64 --decode)"
+eval server_access_token_lifetime="$(jq -r '.server_access_token_lifetime | @sh' $config_file_path)"
+eval server_refresh_token_lifetime="$(jq -r '.server_refresh_token_lifetime | @sh' $config_file_path)"
+eval mongo_root_username="$(jq -r '.mongo_root_username | @sh' $config_file_path | base64 --decode)"
+eval mongo_root_password="$(jq -r '.mongo_root_password | @sh' $config_file_path | base64 --decode)"
 
 
 envsubst < $repository_root_dir/kubernetes/mongodb/2-mongodb-secrets.yaml | sudo tee $repository_root_dir/kubernetes/mongodb/2-mongodb-secrets.yaml

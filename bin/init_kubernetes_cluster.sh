256aec144498240a8c8e86ba714ca3cb82c97379
rMiccolis <r.miccolis1@gmail.com>
2023-04-27 16:14:33 +0200

    wait for pods to be running

--- bin/init_kubernetes_cluster.sh
+++ bin/init_kubernetes_cluster.sh
@@ -19,5 +19,5 @@ kubectl apply -f calico.yaml
 
 sleep 10
 
-kubectl wait --for=condition=Ready --all pods --all-namespaces &
+kubectl wait --for=condition=ContainersReady --all pods --all-namespaces &
 wait

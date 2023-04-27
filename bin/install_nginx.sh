008affc86c88d456fe79c5ff5303bd045cd1fd9a
rMiccolis <r.miccolis1@gmail.com>
2023-04-27 21:53:37 +0200

    standard nginx installation

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -7,13 +7,15 @@ echo -e "${LCYAN}Installing NGINX to be reachble on $master_host_ip.${WHITE}"
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-cat <<EOF | sudo tee nginx_helm_config.yaml
+cat << EOF | sudo tee nginx_helm_config.yaml
 controller:
   service:
     externalIPs: [$master_host_ip]
 EOF
 
-helm install --namespace ingress-nginx --create-namespace ingress-nginx ingress-nginx/ingress-nginx -f nginx_helm_config.yaml
+kubectl create namespace ingress-nginx
+# helm install --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx -f nginx_helm_config.yaml
+helm install --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx
 kubectl wait --for=condition=ContainersReady --all pods -n ingress-nginx --timeout=1200s &
 wait
 echo -e "${LBLUE}Nginx successfully installed with Helm!${WHITE}"

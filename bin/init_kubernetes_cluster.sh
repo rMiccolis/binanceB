854c8f48dc9ad31b8cece6f566390ec6d6fbb41b
rMiccolis <r.miccolis1@gmail.com>
2023-05-02 11:46:38 +0200

    fall back to calico 3.25.0

--- bin/init_kubernetes_cluster.sh
+++ bin/init_kubernetes_cluster.sh
@@ -14,13 +14,13 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 #install calico CNI to kubernetes cluster:
 echo -e "${LBLUE}Installing calico CNI to kubernetes cluster${WHITE}"
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 # kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
 # kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml
 # watch kubectl get pods -n calico-system
 
-sleep 10
+sleep 100
 
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1200s &
 wait

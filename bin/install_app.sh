ae7695a83cabfaa9d52de631569db7be7af65788
rMiccolis <r.miccolis1@gmail.com>
2023-05-03 21:28:25 +0200

    fix new line in secrets and config yaml

--- bin/install_app.sh
+++ bin/install_app.sh
@@ -6,11 +6,11 @@ eval cluster_dns_name="$(jq -r '.cluster_dns_name | @sh' $config_file_path)"
 export cluster_dns_name=$cluster_dns_name
 
 eval server_access_token_secret="$(jq -r '.server_access_token_secret | @sh' $config_file_path)"
-server_access_token_secret=$(echo $server_access_token_secret | base64)
+server_access_token_secret=$(echo -n $server_access_token_secret | base64)
 export server_access_token_secret=$server_access_token_secret
 
 eval server_refresh_token_secret="$(jq -r '.server_refresh_token_secret | @sh' $config_file_path)"
-server_refresh_token_secret=$(echo $server_refresh_token_secret | base64)
+server_refresh_token_secret=$(echo -n $server_refresh_token_secret | base64)
 export server_refresh_token_secret=$server_refresh_token_secret
 
 eval server_access_token_lifetime="$(jq -r '.server_access_token_lifetime | @sh' $config_file_path)"
@@ -20,11 +20,11 @@ eval server_refresh_token_lifetime="$(jq -r '.server_refresh_token_lifetime | @s
 export server_refresh_token_lifetime=$server_refresh_token_lifetime
 
 eval mongo_root_username="$(jq -r '.mongo_root_username | @sh' $config_file_path)"
-mongo_root_username=$(echo $mongo_root_username | base64)
+mongo_root_username=$(echo -n $mongo_root_username | base64)
 export mongo_root_username=$mongo_root_username
 
 eval mongo_root_password="$(jq -r '.mongo_root_password | @sh' $config_file_path)"
-mongo_root_password=$(echo $mongo_root_password | base64)
+mongo_root_password=$(echo -n $mongo_root_password | base64)
 export mongo_root_password=$mongo_root_password
 
 mkdir /home/$USER/temp
@@ -41,3 +41,4 @@ kubectl apply -f /home/$USER/temp/3-server/
 kubectl apply -f /home/$USER/temp/4-client/
 
 rm -rf /home/$USER/temp
+rm -rf /home/$USER/main_config.json

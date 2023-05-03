071906e553a1aa50c80311063d42f32d098df428
rMiccolis <r.miccolis1@gmail.com>
2023-05-03 15:36:38 +0200

    fix temp path

--- bin/install_app.sh
+++ bin/install_app.sh
@@ -29,10 +29,10 @@ export mongo_root_password=$mongo_root_password
 
 mkdir /home/$USER/temp
 cp -R $repository_root_dir/binanceB/kubernetes/app/* /home/$USER/temp
-envsubst < $repository_root_dir/binanceB/kubernetes/app/2-mongodb/2-mongodb-secrets.yaml | sudo tee /home/$USER/temp/binanceB/kubernetes/app/2-mongodb/2-mongodb-secrets.yaml > /dev/null
-envsubst < $repository_root_dir/binanceB/kubernetes/app/3-server/3-server-secrets.yaml | sudo tee /home/$USER/temp/binanceB/kubernetes/app/3-server/3-server-secrets.yaml > /dev/null
-envsubst < $repository_root_dir/binanceB/kubernetes/app/3-server/4-server-configmap.yaml | sudo tee /home/$USER/temp/binanceB/kubernetes/app/3-server/4-server-configmap.yaml > /dev/null
-envsubst < $repository_root_dir/binanceB/kubernetes/app/4-client/3-client-configmap.yaml | sudo tee /home/$USER/temp/binanceB/kubernetes/app/4-client/3-client-configmap.yaml > /dev/null
+envsubst < $repository_root_dir/binanceB/kubernetes/app/2-mongodb/2-mongodb-secrets.yaml | sudo tee /home/$USER/temp/2-mongodb/2-mongodb-secrets.yaml > /dev/null
+envsubst < $repository_root_dir/binanceB/kubernetes/app/3-server/3-server-secrets.yaml | sudo tee /home/$USER/temp/3-server/3-server-secrets.yaml > /dev/null
+envsubst < $repository_root_dir/binanceB/kubernetes/app/3-server/4-server-configmap.yaml | sudo tee /home/$USER/temp/3-server/4-server-configmap.yaml > /dev/null
+envsubst < $repository_root_dir/binanceB/kubernetes/app/4-client/3-client-configmap.yaml | sudo tee /home/$USER/temp/4-client/3-client-configmap.yaml > /dev/null
 
 echo -e "${GREEN}Starting Application...${WHITE}"
 kubectl apply -f /home/$USER/temp/1-namespaces/

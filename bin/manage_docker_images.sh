78f2a6ed0268d92d3b55dcaedfb6f2f22c2fddde
rMiccolis <r.miccolis1@gmail.com>
2023-05-04 22:01:53 +0200

    choose between production or local IP network

--- bin/manage_docker_images.sh
+++ bin/manage_docker_images.sh
@@ -5,9 +5,16 @@
 # cd into project root directory
 cd $repository_root_dir/binanceB/
 
+eval environment="$(jq -r '.environment | @sh' $config_file_path)"
+cluster_ip=$master_host_ip
+if [ "$environment" == "production" ]; then
+eval cluster_public_ip="$(jq -r '.cluster_public_ip | @sh' $config_file_path)"
+cluster_ip=$cluster_public_ip
+fi
+
 #before building images we have to set a .env file to pass client its environment variables
 cat << EOF | sudo tee $repository_root_dir/binanceB/client/.env.production
-VITE_SERVER_URI="http://$master_host_ip/server/"
+VITE_SERVER_URI="http://$cluster_ip/server/"
 EOF
 
 # Start building docker images

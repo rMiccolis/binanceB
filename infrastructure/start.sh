e236eefbb3a7c48da3339c3736b32dfed7c78bf1
rMiccolis <r.miccolis1@gmail.com>
2023-06-04 11:27:13 +0200

    edit colors

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -23,59 +23,59 @@ export config_file_path=$config_file_path
 
 echo -e "${LGREEN}Starting phase 1 / 10 ===> Setting up host settings and dependencies: $(hostname -I)${WHITE}"
 ./binanceB/bin/set_host_settings.sh
-echo -e "${GREEN}Phase 1 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 1 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 2 / 10 ===> Installing Docker${WHITE}"
 ./binanceB/bin/install_docker.sh
-echo -e "${GREEN}Phase 2 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 2 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Docker Hub login with username: $docker_username${WHITE}";
 # login into docker
 sudo docker login --username $docker_username --password $docker_password > /dev/null 2>&1
-echo -e "${GREEN}Login succeded!!${WHITE}"
+echo -e "${LGREEN}Login succeded!!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 3 / 10 ===> Installing Cri-Docker (Container Runtime Interface)${WHITE}"
 ./binanceB/bin/install_cri_docker.sh
-echo -e "${GREEN}Phase 3 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 3 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 4 / 10 ===> Installing Kubernetes${WHITE}"
 ./binanceB/bin/install_kubernetes.sh
-echo -e "${GREEN}Phase 4 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 4 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 5 / 10 ===> Initialize Kubernetes cluster${WHITE}"
 ./binanceB/bin/init_kubernetes_cluster.sh
-echo -e "${GREEN}Phase 5 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 5 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 6 / 10 ===> Setup worker nodes and joining them to cluster ${WHITE}"
 ./binanceB/bin/setup_worker_nodes.sh
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1800s &
 wait
-echo -e "${GREEN}Phase 6 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 6 / 10 ===> Operation Done!${WHITE}"
 
 echo -e "${LGREEN}Starting phase 7 / 10 ===> Installing Helm (package manager for Kubernetes)${WHITE}"
 ./binanceB/bin/install_helm.sh
-echo -e "${GREEN}Phase 7 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 7 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 8 / 10 ===> Installing Nginx (to be used as a reverse proxy for Kubernetes cluster)${WHITE}"
 ./binanceB/bin/install_nginx.sh
-echo -e "${GREEN}Phase 8 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 8 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 9 / 10 ===> Building server and client docker images and pushing them to docker hub${WHITE}"
 ./binanceB/bin/manage_docker_images.sh
-echo -e "${GREEN}Phase 9 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 9 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Starting phase 10 / 10 ===> Applying configuration file and deployng the application to the cluster${WHITE}"
 ./binanceB/bin/install_app.sh
-echo -e "${GREEN}Phase 10 / 10 ===> Operation Done!${WHITE}"
+echo -e "${LGREEN}Phase 10 / 10 ===> Operation Done!${WHITE}"
 
 
 echo -e "${LGREEN}Waiting for the Application to get started...${WHITE}"

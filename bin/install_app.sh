3685da29b2a064063a8fd4640d3adeeffc662cf4
rMiccolis <r.miccolis1@gmail.com>
2023-06-15 15:15:14 +0200

    wait for mongodb

--- bin/install_app.sh
+++ bin/install_app.sh
@@ -27,6 +27,8 @@ envsubst < $repository_root_dir/binanceB/kubernetes/app/4-client/4-client-deploy
 echo -e "${LBLUE}Starting Application...${WHITE}"
 kubectl apply -f /home/$USER/temp/1-namespaces/
 kubectl apply -f /home/$USER/temp/2-mongodb/
+kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1800s &
+wait
 kubectl apply -f /home/$USER/temp/3-server/
 kubectl apply -f /home/$USER/temp/4-client/
 

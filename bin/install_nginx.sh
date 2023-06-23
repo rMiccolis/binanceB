8b54654ad22c4e2b5123ebd63fe8b5b4e3f9ac76
rMiccolis <r.miccolis1@gmail.com>
2023-06-23 09:33:16 +0200

    edit nginx config

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -13,6 +13,7 @@ controller:
   config:
     enable-real-ip: true
     use-forwarded-headers: true
+    proxy-real-ip-cidr: "37.182.190.24/32"
     use-gzip: true
     generate-request-id: true
     proxy-body-size: 1024k
@@ -20,6 +21,8 @@ controller:
     client-header-buffer-size: 1024k
     error-log-level: warn
     http2-max-header-size: 1024k
+    log-format-escape-json: "true"
+    log-format-upstream: '{"time": "$time_iso8601", "proxy_protocol_addr": "$proxy_protocol_addr", "proxy_add_x_forwarded_for": "$proxy_add_x_forwarded_for", "remote_addr": "$remote_addr", "x_forwarded_for": "$x_forwarded_for" }'
   autoscaling:
     enabled: true
     minReplicas: 1

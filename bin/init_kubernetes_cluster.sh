5ceac221cb0ca042c431bd69f372945fd63f8189
rMiccolis <r.miccolis1@gmail.com>
2023-06-26 16:26:46 +0200

    WIP: fix join control plane node

--- bin/init_kubernetes_cluster.sh
+++ bin/init_kubernetes_cluster.sh
@@ -3,15 +3,10 @@
 ###############################################################################
 # Init kubeadm cluster
 echo -e "${LBLUE}Init kubeadm cluster${WHITE}"
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock --control-plane-endpoint=$master_host_ip --upload-certs #> /dev/null 2>&1
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock --control-plane-endpoint=$master_host_ip --upload-certs > certs.txt #> /dev/null 2>&1
 
 mkdir -p $HOME/.kube
 
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#install calico CNI to kubernetes cluster:
-echo -e "${LBLUE}Installing calico CNI to kubernetes cluster${WHITE}"
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O > /dev/null 2>&1
-kubectl apply -f calico.yaml > /dev/null 2>&1

95b4053256f37b91038ea185911a640e466900bc
rMiccolis <r.miccolis1@gmail.com>
2023-06-28 14:34:53 +0200

    added strictARP to kube-proxy

--- bin/install_metallb.sh
+++ bin/install_metallb.sh
@@ -4,6 +4,13 @@ echo -e "${LBLUE}Installing METAL LB${WHITE}"
 
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1800s &
 wait
+
+# If you’re using kube-proxy in IPVS mode, since Kubernetes v1.14.2 you have to enable strict ARP mode.
+# Note, you don’t need this if you’re using kube-router as service-proxy because it is enabling strict ARP by default.
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1800s &
 wait

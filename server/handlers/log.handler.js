a60d14637c808acd18b3a7cd0e305e0e7add8f6d
rMiccolis <r.miccolis1@gmail.com>
2023-07-07 12:13:07 +0200

    fix colors

--- server/handlers/log.handler.js
+++ server/handlers/log.handler.js
@@ -1,21 +1,21 @@
 let logWarning = (message) => {
   //yellow message
-  console.log(`\x1b[33m${JSON.stringify(message, null, 4)}`)
+  console.log(`\x1b[33m${JSON.stringify(message, null, 4)} \x1b[37m`)
 }
 
 let logError = (message) => {
   //red message
-  console.log(`\x1b[31m${JSON.stringify(message, null, 4)}`)
+  console.log(`\x1b[31m${JSON.stringify(message, null, 4)} \x1b[37m`)
 }
 
 let logInfo = (message) => {
   //green message
-  console.log(`\x1b[32m${JSON.stringify(message, null, 4)}`)
+  console.log(`\x1b[32m${JSON.stringify(message, null, 4)} \x1b[37m`)
 }
 
 let logDebug = (message) => {
   //blue message
-  console.log(`\x1b[34m${JSON.stringify(message, null, 4)}`)
+  console.log(`\x1b[34m${JSON.stringify(message, null, 4)} \x1b[37m`)
 }
 
 module.exports = {

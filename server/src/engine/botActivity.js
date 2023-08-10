cd26d306bc544a9532e823c1e9ce38fb5e2cd0ce
rMiccolis <r.miccolis1@gmail.com>
2023-08-10 14:16:42 +0200

    edit print

--- server/src/engine/botActivity.js
+++ server/src/engine/botActivity.js
@@ -8,7 +8,7 @@ const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 const main = async () => {
     try {
         console.log("This was a test and the job is running!")
-        console.log(":")
+        console.log("these are the running pods:")
         const podsRes = await k8sApi.listNamespacedPod('binance-b', pretty="true");
         console.log(podsRes.body.items.map(el => el.metadata.name));
         console.log(process.env);

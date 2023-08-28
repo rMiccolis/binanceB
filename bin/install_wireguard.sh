56a04ea5e1d9bb95bf3e66e95ac87a174bac1920
rMiccolis <r.miccolis1@gmail.com>
2023-08-28 21:41:00 +0200

    WIP: fix vpn peer config

--- bin/install_wireguard.sh
+++ bin/install_wireguard.sh
@@ -31,6 +31,7 @@ master_host_ip_vpn=""
 mkdir wireguard_keys
 cd wireguard_keys
 
+echo -e "${LBLUE}Installing Wireguard...${WHITE}"
 sudo apt install wireguard -y
 umask 077
 
@@ -41,6 +42,7 @@ host_username=${host_string[0]}
 host_ip=${host_string[1]}
 host_ip_vpn=${host_string[2]}
 
+echo -e "${LBLUE}Generating keys for $host_username ${WHITE}"
 wg genkey | tee ${host_username}_privatekey | wg pubkey > ${host_username}_publickey
 
 if [ "${host_username}" == "m1" ]; then
@@ -52,6 +54,7 @@ master_host_name=$host_username
 sudo chown root:${host_username} /etc/wireguard/
 sudo chmod -R 770 /etc/wireguard/
 
+echo -e "${LBLUE}Generating Configuration for $host_username ${WHITE}"
 sudo cat << EOF | tee /etc/wireguard/wg0.conf > /dev/null
 [Interface]
 Address = ${host_ip_vpn}/16
@@ -60,6 +63,7 @@ PrivateKey = $(cat ${host_username}_privatekey)
 
 EOF
 
+echo -e "${LBLUE}Activating wg0 Interface for $host_username ${WHITE}"
 sudo wg-quick up wg0
 
 else
@@ -67,11 +71,13 @@ else
 ssh-keyscan $host_ip >> ~/.ssh/known_hosts &
 wait
 
+echo -e "${LBLUE}Installing Wireguard for $host_username ${WHITE}"
 ssh ${host_username}@$host_ip "sudo apt install wireguard -y" &
 wait
 ssh ${host_username}@$host_ip "umask 077" &
 wait
 
+echo -e "${LBLUE}Generating Configuration for $host_username ${WHITE}"
 sudo cat << EOF | tee -a /etc/wireguard/${host_username}_wg0.conf > /dev/null
 [Interface]
 ListenPort = 51820
@@ -85,19 +91,22 @@ AllowedIPs = 10.10.0.0/16
 PersistentKeepalive = 30
 EOF
 
-wg set wg0 peer "$(cat ${host_username}_publickey)" allowed-ips ${host_ip_vpn}/32
-ip -4 route add ${host_ip_vpn}/32 dev wg0
-
 ssh ${host_username}@$host_ip "sudo chown root:${host_username} /etc/wireguard/" &
 wait
 ssh ${host_username}@$host_ip "sudo chmod -R 770 /etc/wireguard/" &
 wait
 
+echo -e "${LBLUE}Sending peer Configuration to $host_username ${WHITE}"
 scp -q /etc/wireguard/${host_username}_wg0.conf ${host_username}@$host_ip:/etc/wireguard/wg0.conf &
 wait
 
+echo -e "${LBLUE}Applying peer Configuration to $host_username ${WHITE}"
 ssh ${host_username}@$host_ip "sudo wg-quick up wg0" &
 wait
+
+echo -e "${LBLUE}Adding peer $host_username to server Configuration ${WHITE}"
+wg set wg0 peer "$(cat ${host_username}_publickey)" allowed-ips ${host_ip_vpn}/32
+ip -4 route add ${host_ip_vpn}/32 dev wg0
 fi
 
 done

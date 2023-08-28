5d015956aad981d782dae3e1f3248d19d3300a8f
rMiccolis <r.miccolis1@gmail.com>
2023-08-28 22:59:55 +0200

    removed error not printing when installing nginx

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -2,15 +2,15 @@
 
 echo -e "${LBLUE}Installing NGINX to be reachble on $master_host_ip.${WHITE}"
 #add nginx helm repository (kubernetes version)
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx > /dev/null 2>&1
-helm repo update > /dev/null 2>&1
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx > /dev/null
+helm repo update > /dev/null
 externalIPs="$master_host_ip"
 
 # externalIPs: [$master_host_ip $control_plane_hosts_string]
 
 #tcp:
 #  27017: "mongodb/mongodb:27017" => tcp_port_to_expose: namespace/service_name:service_port
-cat << EOF | tee nginx_helm_config.yaml > /dev/null 2>&1
+cat << EOF | tee nginx_helm_config.yaml > /dev/null
 tcp:
   27017: "mongodb/mongodb:27017"
 controller:
@@ -36,7 +36,7 @@ controller:
     targetMemoryUtilizationPercentage: 50
 EOF
 
-cat << 'EOF' | tee -a nginx_helm_config.yaml > /dev/null 2>&1
+cat << 'EOF' | tee -a nginx_helm_config.yaml > /dev/null
   config:
     use-forwarded-headers: true
     use-gzip: true
@@ -47,7 +47,7 @@ cat << 'EOF' | tee -a nginx_helm_config.yaml > /dev/null 2>&1
 EOF
 
 kubectl create namespace ingress-nginx
-helm install --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx -f nginx_helm_config.yaml > /dev/null 2>&1
+helm install --namespace ingress-nginx ingress-nginx ingress-nginx/ingress-nginx -f nginx_helm_config.yaml > /dev/null
 # to expose port 27017 with nginx
 # --set tcp.PORT="namespace/service_name:PORT"
 # --set tcp.27017="mongodb/mongodb:27017"

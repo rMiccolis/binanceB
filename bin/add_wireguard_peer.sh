9e96895f2afca34e95c2caea2db148aa02794793
rMiccolis <r.miccolis1@gmail.com>
2023-08-29 14:58:12 +0200

    fix adding peer

--- bin/add_wireguard_peer.sh
+++ bin/add_wireguard_peer.sh
@@ -47,8 +47,6 @@ ListenPort = 51820
 Address = $peer_ip/24
 PrivateKey = $(cat ${peer_name}_privatekey)
 DNS = 8.8.8.8
-PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
-PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
 
 [Peer]
 PublicKey = $(cat /home/$USER/wireguard/keys/${master_host_name}_publickey)

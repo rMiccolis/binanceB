764ca1a17e2c4d229b4fd659c98e34aee6abdbc6
rMiccolis <r.miccolis1@gmail.com>
2023-08-29 17:15:30 +0200

    fix config file creation

--- bin/install_wireguard.sh
+++ bin/install_wireguard.sh
@@ -138,8 +138,18 @@ ssh ${host_username}@$host_ip "sudo wg-quick up wg0" &
 wait
 
 echo -e "${LBLUE}Adding peer $host_username to server Configuration ${WHITE}"
-sudo wg set wg0 peer "$(cat ${host_username}_publickey)" allowed-ips ${host_ip_vpn}/32
-sudo ip -4 route add ${host_ip_vpn}/32 dev wg0
+# sudo wg set wg0 peer "$(cat ${host_username}_publickey)" allowed-ips ${host_ip_vpn}/32
+# sudo ip -4 route add ${host_ip_vpn}/32 dev wg0
+sudo cat << EOF | tee -a /etc/wireguard/wg0.conf > /dev/null
+[Peer]
+# ${host_username}
+PublicKey = "$(cat ${host_username}_publickey)"
+AllowedIPs = ${host_ip_vpn}/32
+
+EOF
+
+systemctl reload wg-quick@wg0
+
 fi
 
 done

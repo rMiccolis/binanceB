fc17e4f50cd9631319e0bad81fd003dc90e6bfd1
rMiccolis <r.miccolis1@gmail.com>
2023-09-23 19:08:42 +0200

    restart coredns

--- bin/init_kubernetes_cluster.sh
+++ bin/init_kubernetes_cluster.sh
@@ -63,3 +63,5 @@ kubectl apply -f calico.yaml > /dev/null 2>&1
 
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=3000s &
 wait
+
+kubectl -n kube-system rollout restart deployment coredns

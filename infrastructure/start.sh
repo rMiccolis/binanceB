31610cbb963cf08837e98ea3490e399282184706
rMiccolis <r.miccolis1@gmail.com>
2023-10-05 11:39:34 +0200

    wip deny peers packet forwarding

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -66,77 +66,77 @@ echo -e "${LGREEN}Starting phase 1 / 10 ===> Setting up host settings and depend
 echo -e "${LGREEN}Phase 1 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 2 / 10 ===> Installing Docker${WHITE}"
-./binanceB/bin/install_docker.sh
-echo -e "${LGREEN}Phase 2 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 2 / 10 ===> Installing Docker${WHITE}"
+# ./binanceB/bin/install_docker.sh
+# echo -e "${LGREEN}Phase 2 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Docker Hub login with username: $docker_username${WHITE}";
-# login into docker
-sudo docker login --username $docker_username --password $docker_access_token > /dev/null 2>&1
-echo -e "${LGREEN}Login succeded!!${WHITE}"
+# echo -e "${LGREEN}Docker Hub login with username: $docker_username${WHITE}";
+# # login into docker
+# sudo docker login --username $docker_username --password $docker_access_token > /dev/null 2>&1
+# echo -e "${LGREEN}Login succeded!!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 3 / 10 ===> Installing Cri-Docker (Container Runtime Interface)${WHITE}"
-./binanceB/bin/install_cri_docker.sh
-echo -e "${LGREEN}Phase 3 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 3 / 10 ===> Installing Cri-Docker (Container Runtime Interface)${WHITE}"
+# ./binanceB/bin/install_cri_docker.sh
+# echo -e "${LGREEN}Phase 3 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 4 / 10 ===> Installing Kubernetes${WHITE}"
-./binanceB/bin/install_kubernetes.sh
-echo -e "${LGREEN}Phase 4 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 4 / 10 ===> Installing Kubernetes${WHITE}"
+# ./binanceB/bin/install_kubernetes.sh
+# echo -e "${LGREEN}Phase 4 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 5 / 10 ===> Initialize Kubernetes cluster${WHITE}"
-./binanceB/bin/init_kubernetes_cluster.sh
-echo -e "${LGREEN}Phase 5 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 5 / 10 ===> Initialize Kubernetes cluster${WHITE}"
+# ./binanceB/bin/init_kubernetes_cluster.sh
+# echo -e "${LGREEN}Phase 5 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 6 / 10 ===> Setup worker nodes and joining them to cluster ${WHITE}"
-./binanceB/bin/setup_worker_nodes.sh
-echo -e "${LGREEN}Phase 6 / 10 ===> Operation Done!${WHITE}"
-kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=3000s &
-wait
+# echo -e "${LGREEN}Starting phase 6 / 10 ===> Setup worker nodes and joining them to cluster ${WHITE}"
+# ./binanceB/bin/setup_worker_nodes.sh
+# echo -e "${LGREEN}Phase 6 / 10 ===> Operation Done!${WHITE}"
+# kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=3000s &
+# wait
 
-echo -e "${LGREEN}Starting phase 7 / 10 ===> Installing Helm (package manager for Kubernetes)${WHITE}"
-./binanceB/bin/install_helm.sh
-echo -e "${LGREEN}Phase 7 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 7 / 10 ===> Installing Helm (package manager for Kubernetes)${WHITE}"
+# ./binanceB/bin/install_helm.sh
+# echo -e "${LGREEN}Phase 7 / 10 ===> Operation Done!${WHITE}"
 
-echo -e "${LGREEN}Starting EXTRA phase ===> Installing metallb${WHITE}"
-./binanceB/bin/install_metallb.sh
-echo -e "${LGREEN}EXTRA Phase ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting EXTRA phase ===> Installing metallb${WHITE}"
+# ./binanceB/bin/install_metallb.sh
+# echo -e "${LGREEN}EXTRA Phase ===> Operation Done!${WHITE}"
 
-echo -e "${LGREEN}Starting phase 8 / 10 ===> Installing Nginx (to be used as a reverse proxy for Kubernetes cluster)${WHITE}"
-./binanceB/bin/install_nginx.sh
-echo -e "${LGREEN}Phase 8 / 10 ===> Operation Done!${WHITE}"
-
-
-echo -e "${LGREEN}Starting phase 9 / 10 ===> Building server and client docker images and pushing them to docker hub${WHITE}"
-./binanceB/bin/manage_docker_images.sh
-echo -e "${LGREEN}Phase 9 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 8 / 10 ===> Installing Nginx (to be used as a reverse proxy for Kubernetes cluster)${WHITE}"
+# ./binanceB/bin/install_nginx.sh
+# echo -e "${LGREEN}Phase 8 / 10 ===> Operation Done!${WHITE}"
 
 
-echo -e "${LGREEN}Starting phase 10 / 10 ===> Applying configuration file and deployng the application to the cluster${WHITE}"
-./binanceB/bin/install_app.sh
-echo -e "${LGREEN}Phase 10 / 10 ===> Operation Done!${WHITE}"
+# echo -e "${LGREEN}Starting phase 9 / 10 ===> Building server and client docker images and pushing them to docker hub${WHITE}"
+# ./binanceB/bin/manage_docker_images.sh
+# echo -e "${LGREEN}Phase 9 / 10 ===> Operation Done!${WHITE}"
 
-end_time="$(date -u +%s)"
-elapsed_time=$(($end_time-$start_time))
-elapsed_time=$(($elapsed_time/60))
-echo -e "${LGREEN}Elapsed time:'$(($elapsed_time))' minutes ${WHITE}"
 
-echo -e "${LGREEN}Waiting for the Application to get started...${WHITE}"
-kubectl wait --for=condition=Ready --all pods --all-namespaces --timeout=3000s &
-wait
+# echo -e "${LGREEN}Starting phase 10 / 10 ===> Applying configuration file and deployng the application to the cluster${WHITE}"
+# ./binanceB/bin/install_app.sh
+# echo -e "${LGREEN}Phase 10 / 10 ===> Operation Done!${WHITE}"
 
-kubectl -n kube-system rollout restart deployment coredns
+# end_time="$(date -u +%s)"
+# elapsed_time=$(($end_time-$start_time))
+# elapsed_time=$(($elapsed_time/60))
+# echo -e "${LGREEN}Elapsed time:'$(($elapsed_time))' minutes ${WHITE}"
 
-kubectl wait --for=condition=Ready --all pods --all-namespaces --timeout=2000s &
-wait
+# echo -e "${LGREEN}Waiting for the Application to get started...${WHITE}"
+# kubectl wait --for=condition=Ready --all pods --all-namespaces --timeout=3000s &
+# wait
 
-echo -e "${LGREEN}Application is correctly running!${WHITE}"
-echo -e "${LGREEN}Check it out at http://$app_server_addr/${WHITE}"
-end_time="$(date -u +%s)"
-elapsed_time=$(($end_time-$start_time))
-elapsed_time=$(($elapsed_time/60))
-echo -e "${LGREEN}Elapsed time:'$(($elapsed_time))' minutes ${WHITE}"
+# kubectl -n kube-system rollout restart deployment coredns
+
+# kubectl wait --for=condition=Ready --all pods --all-namespaces --timeout=2000s &
+# wait
+
+# echo -e "${LGREEN}Application is correctly running!${WHITE}"
+# echo -e "${LGREEN}Check it out at http://$app_server_addr/${WHITE}"
+# end_time="$(date -u +%s)"
+# elapsed_time=$(($end_time-$start_time))
+# elapsed_time=$(($elapsed_time/60))
+# echo -e "${LGREEN}Elapsed time:'$(($elapsed_time))' minutes ${WHITE}"

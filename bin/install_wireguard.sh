ab845fbe52f7f01a5a484332898cb3b4fcb84758
rMiccolis <r.miccolis1@gmail.com>
2023-10-05 11:16:59 +0200

    wip deny peers packet forwarding

--- bin/install_wireguard.sh
+++ bin/install_wireguard.sh
@@ -104,9 +104,8 @@ Address = ${host_ip_vpn}/24
 ListenPort = 51820
 PrivateKey = $(cat ${host_username}_privatekey)
 SaveConfig = true
-PostUp = iptables -A FORWARD -i %i -j DROP; iptables -A FORWARD -m iprange --src-range 10.11.1.1-10.11.1.3 -i %i -j ACCEPT; iptables -A FORWARD -m iprange --src-range 10.11.1.50-10.11.1.200 -i %i -d 192.168.1.0/24 -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
-PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
-
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A -s 10.11.1.51 POSTROUTING -o eth0 -j MASQUERADE;
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D -s 10.11.1.51 POSTROUTING -o eth0 -j MASQUERADE;
 EOF
 
 echo -e "${LBLUE}Activating wg0 Interface for $host_username and Enable IP Forwarding${WHITE}"

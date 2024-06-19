d07faf4fe3846111b887a7ac8344b1c850e88f07
rMiccolis <r.miccolis1@gmail.com>
2024-06-19 13:31:56 +0200

    save output on file

--- bin/install_kubernetes.sh
+++ bin/install_kubernetes.sh
@@ -9,23 +9,23 @@ sudo /home/$USER/binanceB/bin/disable_swap.sh
 # install kubeadm, kubelet and kubectl:
 # Update the apt package index and install packages needed to use the Kubernetes apt repository:
 echo -e "${LBLUE}Update the apt package index and install packages needed to use the Kubernetes apt repository${WHITE}"
-sudo apt-get update > /dev/null # 2>&1
-sudo apt-get upgrade -y -q > /dev/null # 2>&1
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg > /dev/null # 2>&1
+sudo apt-get update > /dev/null >> /home/$USER/output.txt # 2>&1
+sudo apt-get upgrade -y -q > /dev/null >> /home/$USER/output.txt# 2>&1
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg > /dev/null >> /home/$USER/output.txt# 2>&1
 
 # Download the Google Cloud public signing key:
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v${kubernetes_version}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v${kubernetes_version}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >> /home/$USER/output.txt
 # Add the Kubernetes apt repository:
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null # 2>&1
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null >> /home/$USER/output.txt # 2>&1
 
 # Update apt package index, install latest versions of kubelet, kubeadm and kubectl, and pin their version:
 echo -e "${LBLUE}Update apt package index, install latest versions of kubelet, kubeadm and kubectl, and pin their version${WHITE}"
-sudo apt-get update > /dev/null # 2>&1
+sudo apt-get update > /dev/null >> /home/$USER/output.txt # 2>&1
 
 
-sudo apt-get install -y -q kubelet kubeadm kubectl > /dev/null # 2>&1
+sudo apt-get install -y -q kubelet kubeadm kubectl > /dev/null >> /home/$USER/output.txt # 2>&1
 # sudo apt-get install -y kubelet=1.27.1-00 kubeadm=1.27.1-00 kubectl=1.27.1-00
-sudo apt-mark hold kubelet kubeadm kubectl > /dev/null # 2>&1
+sudo apt-mark hold kubelet kubeadm kubectl > /dev/null >> /home/$USER/output.txt # 2>&1
 # Enable the kubelet service before running kubeadm
-sudo systemctl enable --now kubelet
+sudo systemctl enable --now kubelet >> /home/$USER/output.txt
 ###############################################################################

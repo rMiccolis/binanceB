c8bb504f5ff1f8d3e58af7137578c50472f4277a
rMiccolis <r.miccolis1@gmail.com>
2025-01-31 14:20:32 +0100

    Revert "update metallb version"
    This reverts commit d02abe20f3e47698eb81381e3fc383dfb74a9b08.

--- bin/install_metallb.sh
+++ bin/install_metallb.sh
@@ -11,8 +11,7 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
 kubectl wait --for=condition=ContainersReady --all pods --all-namespaces --timeout=1800s &
 wait
 echo -e "${LBLUE}METAL LB successfully installed with Helm!${WHITE}"
